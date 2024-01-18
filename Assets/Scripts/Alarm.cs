8b810a8051c107a9457c0ff1b3782f7246d5eb39
Voltru24 <114227692+Voltru24@users.noreply.github.com>
2024-01-18 23:05:31 +0300

    1

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,31 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private float _maxVolume;
-    private float _minVolume = 0;
+    [SerializeField] private float _speedVolume;
 
+    private float _minVolume = 0;
+    private AudioSource _audioSource;
     private bool _isPenetration = false;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.GetComponent<Rogue>())
+        {
+            _isPenetration = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rogue>())
+        {
+            _isPenetration = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        if (_isPenetration)
+        if (_isPenetration == true)
         {
-             
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, _speedVolume * Time.deltaTime);
         }
         else
         {
-
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, _speedVolume * Time.deltaTime);
         }
-        
     }
-
-    
 }
